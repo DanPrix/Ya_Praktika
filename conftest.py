05e4f78c3ce62498e9ab8a54b14be13fb4a3d451
DanPrix <74776300+DanPrix@users.noreply.github.com>
2025-01-25 12:54:04 +0300

    Sprint diplom 3 init

--- conftest.py
+++ conftest.py
@@ -1,32 +1,68 @@
-import requests
 import pytest
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions
+from selenium.webdriver.support.wait import WebDriverWait
+from urls.urls import REGISTER_URL, LOGIN_URL, BASE_URL
 from faker import Faker
-from urls.urls import CREATE_USER_URL, INGREDIENTS_URL
-from random import randrange
+
+ANY_INPUT_LOCATOR = ".//main//form//input"
+EMAIL_OR_NAME_LOCATOR = ".//main//form//input[@name='name']"
+PASSWORD_LOCATOR = ".//main//form//input[@name='Пароль']"
+REGISTER_BUTTON_LOCATOR = ".//main//form/button"
+EMAIL_LOCATOR = ".//main//form//input[@name='name']"
+LOGIN_BUTTON_LOCATOR = ".//button[text()='Войти']"
 
 
 @pytest.fixture()
-def user():
-    fake = Faker("ru_RU")
-    payload = {
-        "email": fake.email(),
-        "password": fake.password(),
-        "name": fake.name()
-    }
-    response = requests.post(CREATE_USER_URL, data=payload)
-    body = response.json()
-    return {
-        **payload,
-        "accessToken": body["accessToken"],
-        "refreshToken": body["refreshToken"],
-    }
+def random_email():
+    return Faker().email()
 
 
 @pytest.fixture()
-def random_ingredients():
-    response = requests.get(INGREDIENTS_URL)
-    ingredients = response.json()["data"]
-    buns = list(filter(lambda ing: ing["type"] == "bun", ingredients))
-    mains = list(filter(lambda ing: ing["type"] == "main", ingredients))
-    sauces = list(filter(lambda ing: ing["type"] == "sauce", ingredients))
-    return [buns[randrange(len(buns))]["_id"], mains[randrange(len(mains))]["_id"], sauces[randrange(len(sauces))]["_id"]]
+def random_password():
+    return Faker().password()
+
+
+@pytest.fixture(params=["chrome"])
+def driver(request):
+    browser = request.param
+    if browser == "chrome":
+        driver = webdriver.Chrome()
+    elif browser == "firefox":
+        driver = webdriver.Firefox()
+    else:
+        raise ValueError()
+    yield driver
+    driver.quit()
+
+
+@pytest.fixture()
+def driver_registered(driver, random_email, random_password):
+    driver.get(REGISTER_URL)
+    WebDriverWait(driver, 3).until(
+        expected_conditions.visibility_of_element_located((By.XPATH, ANY_INPUT_LOCATOR)))
+    for input_element in driver.find_elements(By.XPATH, EMAIL_OR_NAME_LOCATOR):
+        input_element.send_keys(random_email)
+    driver.find_element(By.XPATH, PASSWORD_LOCATOR).send_keys(random_password)
+    WebDriverWait(driver, 3).until(
+        expected_conditions.visibility_of_element_located((By.XPATH, ANY_INPUT_LOCATOR)))
+    driver.find_element(By.XPATH, REGISTER_BUTTON_LOCATOR).click()
+    WebDriverWait(driver, 3).until(
+        expected_conditions.url_to_be(LOGIN_URL))
+    yield driver
+    driver.quit()
+
+
+@pytest.fixture()
+def driver_logged_in(driver_registered, random_email, random_password):
+    driver_registered.get(LOGIN_URL)
+    WebDriverWait(driver_registered, 3).until(
+        expected_conditions.url_to_be(LOGIN_URL))
+    driver_registered.find_element(By.XPATH, EMAIL_LOCATOR).send_keys(random_email)
+    driver_registered.find_element(By.XPATH, PASSWORD_LOCATOR).send_keys(random_password)
+    driver_registered.find_element(By.XPATH, LOGIN_BUTTON_LOCATOR).click()
+    WebDriverWait(driver_registered, 3).until(
+        expected_conditions.url_to_be(BASE_URL))
+    yield driver_registered
+    driver_registered.quit()
--- urls/urls.py
+++ urls/urls.py
@@ -1,7 +1,7 @@
-BASE_URL = "https://stellarburgers.nomoreparties.site"
-CREATE_USER_URL = f"{BASE_URL}/api/auth/register"
-LOGIN_URL = f"{BASE_URL}/api/auth/login"
-USER_INFO_URL = f"{BASE_URL}/api/auth/user"
-INGREDIENTS_URL = f"{BASE_URL}/api/ingredients"
-CREATE_ORDER_URL = f"{BASE_URL}/api/orders"
-USER_ORDER_URL = f"{BASE_URL}/api/orders"
+BASE_URL = 'https://stellarburgers.nomoreparties.site/'
+LOGIN_URL = f"{BASE_URL}login"
+FORGOT_PASSWORD_URL = f"{BASE_URL}forgot-password"
+REGISTER_URL = f'{BASE_URL}register'
+PROFILE_URL = f'{BASE_URL}account/profile'
+ORDER_HISTORY_URL = f'{BASE_URL}account/order-history'
+FEED_URL = f'{BASE_URL}feed'
